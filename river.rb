4fbc38b804e0681c5dcf9f30ef5f36d97b818e55
James Budge <thebudgie@gmail.com>
2019-04-09 18:50:52 +0100

    added river class, has name and space for fish

---
+++ river.rb
@@ -0,0 +1,14 @@
+class River
+
+attr_reader :river_name
+
+def initialize(river_name)
+  @river_name = river_name
+  @fish_in_river = []
+end
+
+
+
+
+
+end
--- specs/river_spec.rb
+++ specs/river_spec.rb
@@ -7,7 +7,13 @@ require_relative("../river.rb")
 
 class TestRiver < MiniTest::Test
 
+def setup
+  @river_name = River.new("Spey")
+end
 
+def test_river_has_name
+  assert_equal("Spey", @river_name.river_name)
+end
 
 
 
